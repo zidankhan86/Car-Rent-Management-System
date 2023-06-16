2076466979c19de02dff432eead293290df227b5
zidankhan86 <zidankhan718@gmail.com>
2023-06-16 17:39:34 +0600

    Templeteing

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function dashboard(){
-        return view('backend.master');
+        return view('backend.pages.home');
     }
 }
