2e4c734866e3f09edf1c2efc348f37fa20631676
zidankhan86 <zidankhan718@gmail.com>
2023-06-17 22:35:34 +0600

    change

--- resources/views/backend/fixed/sidebar.blade.php
+++ resources/views/backend/fixed/sidebar.blade.php
@@ -49,7 +49,7 @@
                     Manage Customer
                 </a>
 
-                <a class="nav-link" href="{{route('content.list')}}">
+                <a class="nav-link" href="">
                     <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
                     Upload  Content
                 </a>
